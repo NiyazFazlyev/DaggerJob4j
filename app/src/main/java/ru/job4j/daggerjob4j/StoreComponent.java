1d80b93645e5185cf53e84cc677847d07661db5c
NiyazFazlyev <fazlyev@phystech.edu>
2019-10-15 18:40:57 +0500

    3. Dagger и Singleton.[#188831]

--- app/src/main/java/ru/job4j/daggerjob4j/StoreComponent.java
+++ app/src/main/java/ru/job4j/daggerjob4j/StoreComponent.java
@@ -2,8 +2,11 @@ package ru.job4j.daggerjob4j;
 
 import java.util.List;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = StoreModule.class)
 public interface StoreComponent{
     void injectTo(MainActivity activity);
--- app/src/main/java/ru/job4j/daggerjob4j/StoreModule.java
+++ app/src/main/java/ru/job4j/daggerjob4j/StoreModule.java
@@ -1,10 +1,14 @@
 package ru.job4j.daggerjob4j;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
+
 @Module
 public class StoreModule {
+    @Singleton
     @Provides
     public Store providesStore(){
         return new MemStore();
